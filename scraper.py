0ecfa1893e3856c592238c66643614a7e27eb08b
ansnoussi <anis0snoussi@gmail.com>
2018-11-10 14:09:08 +0000

    scraping_finished

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@
 #scraping this page: https://www.bitcoinblockhalf.com/  to get some information about bitcoin
 
 
-
 from bs4 import BeautifulSoup
 import requests
 page_link ='https://www.bitcoinblockhalf.com/'
@@ -13,7 +12,27 @@ page_response = requests.get(page_link, timeout=5)
 # parse html
 page_content = BeautifulSoup(page_response.content, "html.parser")
 
+####################################################################################
 
-info = page_content.find_all('table', attrs={'class':'table table-striped'})
+#a faster way to scrape:
+#info_table = page_content.find_all('table', attrs={'class':'table table-striped'})
 
-print(info)
+####################################################################################
+
+
+#another more complicated way but more organized
+html_content = list(page_content.children)[4]
+body_content = list(html_content.children)[3]
+container_content = list(body_content.children)[1]
+table_content = list(container_content.children)[9]
+
+#the content I'm more interested in:
+bitcoinInCirculation = list(table_content.children)[1]
+bitcoinToBeProduced = list(table_content.children)[3]
+percentageOfBitcoinMined = list(table_content.children)[5]
+bitcoinLeftToMine = list(table_content.children)[7]
+
+print (list(bitcoinInCirculation)[0].get_text() + list(bitcoinInCirculation)[1].get_text())
+print (list(bitcoinToBeProduced)[0].get_text() + list(bitcoinToBeProduced)[1].get_text())
+print (list(percentageOfBitcoinMined)[0].get_text() + list(percentageOfBitcoinMined)[1].get_text())
+print (list(bitcoinLeftToMine)[0].get_text() + list(bitcoinLeftToMine)[1].get_text())
