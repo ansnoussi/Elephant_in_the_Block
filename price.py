c76221e38d1737ca1ffae60e6c65a413c721c6aa
ansnoussi <anis0snoussi@gmail.com>
2018-11-10 14:12:30 +0000

    fixed_price

--- price.py
+++ price.py
@@ -2,7 +2,7 @@
 
 import requests
 
-print "using coindesk.com/api to get bitcoin api (updated every 1 min)"
+print "using coindesk.com/api to get bitcoin price (updated every 1 min)"
 print ""
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 print("1 BTC = " + r.json()['bpi']['USD']['rate'] + " USD")
